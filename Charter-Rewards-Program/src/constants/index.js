d8694292f51109793bc4c78f69f4265235db796d
selvaganesh241091 <selva.sg24@gmail.com>
2024-09-30 18:45:34 +0530

    Delete Charter-Rewards-Program directory

--- Charter-Rewards-Program/src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'http://localhost:3001/api';
-export const TRANSACTION_API = `${BASE_URL}/transactions`;
--- Charter-Rewards-Program/src/utils/calculate/calculatePoints.test.js
+++
@@ -1,8 +0,0 @@
-import { calculatePoints } from './calculatePoints';
-
-test('calculates points correctly', () => {
-    expect(calculatePoints(120)).toBe(90);
-    expect(calculatePoints(75)).toBe(25);
-    expect(calculatePoints(50)).toBe(0);
-    expect(calculatePoints(200)).toBe(250);
-});
